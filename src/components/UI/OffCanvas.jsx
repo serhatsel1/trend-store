9a43db3f99cdee502c4001d89ad273a983d3faa1
Serhat Sel <selserhat01@gmail.com>
2024-03-09 16:10:46 +0300

    reactPortal

--- src/components/UI/OffCanvas.jsx
+++ src/components/UI/OffCanvas.jsx
@@ -1,19 +1,33 @@
+import ReactDOM from "react-dom";
 import { useContext } from "react";
 import { CartShowContext } from "../../Contexts/CartShowContext";
 import PropTypes from "prop-types";
 import "./OffCanvas.css";
 
-const OffCanvas = ({ children }) => {
+const Backdrop = () => {
   const CartClose = useContext(CartShowContext);
-
   const { setCartIsShow } = CartClose;
+  return <div className="backdrop" onClick={() => setCartIsShow(false)}></div>;
+};
 
+const OffCanvasOverlay = ({ children }) => {
+  return (
+    <div className="offcanvas">
+      <div className="content">{children}</div>
+    </div>
+  );
+};
+
+const OffCanvas = ({ children, setCartIsShow }) => {
+  const portalElement = document.getElementById("overlays");
   return (
     <>
-      <div className="backdrop" onClick={() => setCartIsShow(false)}></div>
-      <div className="offcanvas">
-        <div className="content">{children}</div>
-      </div>
+      {ReactDOM.createPortal(
+        <Backdrop setCartIsShow={setCartIsShow} />,
+        portalElement
+      )}
+      {ReactDOM.createPortal(<OffCanvasOverlay>{children}</OffCanvasOverlay>,portalElement)}
+      
     </>
   );
 };
