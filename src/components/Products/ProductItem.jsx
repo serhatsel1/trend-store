15fae302073c4b0b54d57ea232410d010fd623d8
Serhat Sel <selserhat01@gmail.com>
2024-03-11 00:31:29 +0300

    fetch api

--- src/components/Products/ProductItem.jsx
+++ src/components/Products/ProductItem.jsx
@@ -1,20 +1,43 @@
-import { useContext } from "react";
-import products from "../productData";
+import { useContext, useState } from "react";
+// import products from "../productData";
 
 import "./ProductItem.css";
 import Rating from "./Rating";
 import { CartCRUDContext } from "../../Contexts/CartCRUDContext";
 
 const ProductItem = () => {
-  const productList = products;
+  // const productList = products;
 
   const cartCrudCtx = useContext(CartCRUDContext);
 
   const { addItem } = cartCrudCtx;
+
+  const [products, setProucts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchProductHandler = async () => {
+    setIsLoading(true);
+    const res = await fetch(
+      "https://my-pos-application-api.onrender.com/api/products/get-all"
+    );
+
+    const data = await res.json();
+
+    const newData = data.map((item) => {
+      return {
+        id: item._id,
+        name: item.title,
+        ...item,
+      };
+    });
+
+    await setProucts(newData);
+    setIsLoading(false);
+  };
   return (
     <>
-      {productList?.map((product) => (
-        <li className="card" key={product?.id}>
+      {products?.map((product, i) => (
+        <li className="card" key={i}>
           <img src={product.img} alt={product.name} />
           <h3 className="product-title">{product.name}</h3>
           <p>{product.description}</p>
@@ -27,6 +50,9 @@ const ProductItem = () => {
           </button>
         </li>
       ))}
+      <button className="button" onClick={fetchProductHandler}>
+        {isLoading ? "Loading..." : "fetch api"}
+      </button>
     </>
   );
 };
